5337a01138fe5ff4f7f50173426451bcdacb66d1
jrwebdeveloper1 <jrwebdeveloper1@micandmacsolutions.com>
2026-08-19 13:07:33 +0530

    19-08-2026 changes

--- src/Facades/AskEva.php
+++ src/Facades/AskEva.php
@@ -5,6 +5,19 @@ namespace Askeva\WhatsApp\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static array sendTemplateMessage(array $payload)
+ * @method static array sendTextTemplate(string $to, string $templateName, string $languageCode = 'en')
+ * @method static array sendTextWithVariables(string $to, string $templateName, array $variables, string $languageCode = 'en')
+ * @method static array sendImageTemplate(string $to, string $templateName, string $imageLink, string $languageCode = 'en')
+ * @method static array sendImageWithVariables(string $to, string $templateName, string $imageLink, array $variables, string $languageCode = 'en')
+ * @method static array sendVideoTemplate(string $to, string $templateName, string $videoLink, string $languageCode = 'en')
+ * @method static array sendVideoWithVariables(string $to, string $templateName, string $videoLink, array $variables, string $languageCode = 'en')
+ * @method static array sendDocumentTemplate(string $to, string $templateName, string $documentLink, string $documentFilename, string $languageCode = 'en')
+ * @method static array sendDocumentWithVariables(string $to, string $templateName, string $documentLink, string $documentFilename, array $variables, string $languageCode = 'en')
+ * @method static array sendCarouselTemplate(string $to, string $templateName, array $cards, string $languageCode = 'en')
+ * @method static array sendCarouselWithVariables(string $to, string $templateName, array $cards, string $languageCode = 'en')
+ * @method static array sendAuthenticationTemplate(string $to, string $templateName, string $otp, string $languageCode = 'en')
+ *
  * @see \Askeva\WhatsApp\AskEvaClient
  */
 class AskEva extends Facade
